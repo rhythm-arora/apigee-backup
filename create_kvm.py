dacb5b056a4a064b7f0a512b9ab1c350eede7c98
rhythm-arora <rhythmkrarora@gmail.com>
2020-06-14 22:01:18 +0200

    modify create kvm code

--- create_kvm.py
+++ create_kvm.py
@@ -6,15 +6,10 @@ import requests
 from requests.models import Response
 import json
 
-passcode=''
-org=''
-
 def user_args():
     """
-    Takes input from user like email, password, org and api product name
+    Takes input from user like email, password, org and env
     """
-    global passcode, org
-
     try:
         # Construct the input argument parser
         ap = argparse.ArgumentParser()
@@ -25,6 +20,8 @@ def user_args():
                          help="APIGEE Org Name" )
         ap.add_argument( "-c", "--copykvmenv", required=True,
                          help="Env from which you want to copy KVM" )
+        ap.add_argument( "-n", "--kvmname", required=False,
+                         help="Name of a KVM which you want to copy" )
         ap.add_argument( "-e", "--deploykvmenv", required=True,
                          help="Env in which you want to create KVM" )
 
@@ -38,27 +35,27 @@ def user_args():
 
             # Create b64 encode string
             # Changing the value of global variables passcode and org
-            passcode = b64encode( bytes( args['username'] + ':' + user_pass, "utf-8" ) ).decode( "ascii" )
-            org = args['org']
-            return args
+            args['passcode'] = b64encode( bytes( args['username'] + ':' + user_pass, "utf-8" ) ).decode( "ascii" )
         else:
             print('Incorrect value for environment. Try again')
 
     except Exception as e:
         logging.error( "Exception occurred", exc_info=True )
 
+    return args
+
 
 
 def get_kvm_list_source(user_input):
     """
-    Get the list of all KVM in the source environment of APIGEE ORG
+    Get the list of all KVM in an environment (source) of APIGEE ORG
     """
     app_data = []
     try:
-        env = user_input['copykvmenv']
+        env, org = user_input['copykvmenv'], user_input['org']
         # Creating http request to get app ids
         url = 'https://api.enterprise.apigee.com/v1/organizations/' + org + '/environments/' + env + '/keyvaluemaps'
-        header = {'Authorization': 'Basic ' + passcode}
+        header = {'Authorization': 'Basic ' + user_input['passcode']}
 
         # Making HTTP call and saving the response
         response = requests.get( url, headers=header )  # type: Response
@@ -75,16 +72,16 @@ def get_kvm_list_source(user_input):
 
 def get_kvm_details(user_input, kvm_list_source):
     """
-    Get the values of all KVM from the source envirnment and store it in a list
+    Get the values of all KVM from an environment (source) and store it in a list
     """
     kvm_details = []
     try:
-        env = user_input['copykvmenv']
+        env, org = user_input['copykvmenv'], user_input['org']
         for kvm in kvm_list_source:
             # Creating http request to get app ids
             url = 'https://api.enterprise.apigee.com/v1/organizations/' + org +\
                   '/environments/' + env + '/keyvaluemaps/' + kvm
-            header = {'Authorization': 'Basic ' + passcode}
+            header = {'Authorization': 'Basic ' + user_input['passcode']}
 
             # Making HTTP call and saving the response
             response = requests.get( url, headers=header )  # type: Response
@@ -103,20 +100,20 @@ def get_kvm_details(user_input, kvm_list_source):
 
 def create_kvm_destination(user_input, kvm_details):
     """
-    Creating the KVM in destination environment of APIGEE
+    Creating the KVM in an environment (destination) of APIGEE
     """
-    env = user_input['deploykvmenv']
-    confirm = input("Confirm if you want to create KVM in {} env? Yes or No : ".format(env))
+    env, org = user_input['deploykvmenv'], user_input['org']
+    confirm = input("Confirm if you want to create KVM in {} env? Yes or No : ".format(user_input['deploykvmenv']))
     if confirm.lower() == 'yes':
         try:
-            print( "Creating KVM in {} env\nUpdates\n*******".format( user_input['deploykvmenv'] ) )
+            print("Creating KVM in {} env\nUpdates\n*******".format(user_input['deploykvmenv']))
 
             for kvm in kvm_details:
                 # Creating http request to get app ids
                 url = 'https://api.enterprise.apigee.com/v1/organizations/' + org + \
                       '/environments/' + env + '/keyvaluemaps'
                 header = {'Content-Type': 'application/json',
-                          'Authorization': 'Basic ' + passcode}
+                          'Authorization': 'Basic ' + user_input['passcode']}
 
                 body = json.dumps(kvm)
 
@@ -143,36 +140,38 @@ def main():
         try:
             # Get input from user
             user_input = user_args()
+            if user_input.__contains__('passcode'):
+                print( "Getting KVM List" )
+                # Get KVM List from the source APIGEE environment
+                response, kvm_list_source = get_kvm_list_source(user_input)
 
-            # Get KVM List from the source APIGEE environment
-            print("Getting KVM List")
-            response, kvm_list_source = get_kvm_list_source(user_input)
+                if response.status_code == 200 and len( kvm_list_source ) == 0:
+                    print()
+                    print("Can not find any KVM in {} env. Try again".format(user_input['copykvmenv']))
+                    create_kvm = False
+                elif response.status_code == 200:
+                    print("Total KVMs found : {} in {} env".format(len(kvm_list_source), user_input['copykvmenv']))
+                    print()
+                    print("Now getting KVM data from {} env".format(user_input['copykvmenv']))
 
-            if response.status_code == 200 and len(kvm_list_source) == 0:
-                print()
-                print("Can not find any KVM in {} env. Try again".format(user_input['copykvmenv']))
-                create_kvm = False
-            elif response.status_code == 200:
-                print("Total KVMs found : {} in {} env".format(len(kvm_list_source), user_input['copykvmenv']))
-                print()
-                print("Now getting KVM data from {} env".format(user_input['copykvmenv']))
+                    # Get KVM details
+                    kvm_details = get_kvm_details(user_input, kvm_list_source)
+                    print("Got KVM Details : Count : {}".format(len(kvm_details)))
+                    print()
 
-                # Get KVM details
-                kvm_details = get_kvm_details(user_input, kvm_list_source)
-                print("Got KVM Details : Count : {}".format(len(kvm_details)))
-                print()
+                    # Create KVM
+                    create_kvm_destination(user_input, kvm_details)
+                    create_kvm = False
 
-                # Create KVM
-                create_kvm_destination(user_input, kvm_details)
-                create_kvm = False
+                elif response.status_code == 401:
+                    print( 'Status Code : ' + str( response.status_code ) + '. ' + 'Incorrect email or password.' )
+                    create_kvm = False
+                else:
+                    print( 'Status Code : ' + str( response.status_code ) + '. ' + response.json()['message'] )
+                    create_kvm = False
 
-            elif response.status_code == 401:
-                print( 'Status Code : ' + str( response.status_code ) + '. ' + 'Incorrect email or password.' )
-                create_kvm = False
             else:
-                print( 'Status Code : ' + str( response.status_code ) + '. ' + response.json()['message'] )
                 create_kvm = False
-
         except:
             print("Good Bye")
             create_kvm = False
